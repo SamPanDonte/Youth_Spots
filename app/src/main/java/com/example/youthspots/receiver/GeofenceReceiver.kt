d4cd92b6639c3ec45a7e18fd3760441c9ecb7de6
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2021-06-12 16:20:08 +0200

    geofences receiver added

--- app/src/main/java/com/example/youthspots/receiver/GeofenceReceiver.kt
+++ app/src/main/java/com/example/youthspots/receiver/GeofenceReceiver.kt
@@ -1,9 +1,17 @@
 package com.example.youthspots.receiver
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.example.youthspots.ui.activity.MainActivity
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofenceStatusCodes
 import com.google.android.gms.location.GeofencingEvent
@@ -18,31 +26,52 @@ class GeofenceReceiver : BroadcastReceiver() {
             Log.e("TAG", errorMessage)
             return
         }
-
-        // Get the transition type.
         val geofenceTransition = geofencingEvent.geofenceTransition
 
-        // Test that the reported transition was of interest.
         if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
-        geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
 
-            // Get the geofences that were triggered. A single event can trigger
-            // multiple geofences.
             val triggeringGeofences = geofencingEvent.triggeringGeofences
 
-            // Get the transition details as a String.
-//            val geofenceTransitionDetails = getGeofenceTransitionDetails(
-//                this,
-//                geofenceTransition,
-//                triggeringGeofences
-//            )
+            createChannel(context)
+            if (triggeringGeofences.size == 1) {
+                sendNotification(context, "You entered chosen point of interest!")
+            } else {
+                sendNotification(context, "You entered chosen points of interest!")
+            }
+        }
+    }
 
-            // Send notification and log the transition details.
-//            sendNotification(geofenceTransitionDetails)
-            Log.i("TAG", "SUCCESS")
-        } else {
-            // Log the error.
-            Log.e("TAG", "ERROR")
+    fun sendNotification(context: Context, text: String) {
+        val builder = NotificationCompat.Builder(context, "GeofenceNotification")
+            .setContentTitle("Entered point of interest!")
+            .setContentText(text)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    context,
+                    0,
+                    Intent(context, MainActivity::class.java),
+                    0
+                )
+            )
+        with(NotificationManagerCompat.from(context)) {
+            notify(1, builder.build())
+        }
+    }
+
+    private fun createChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "GeofenceNotification"
+            val descriptionText = "GeofenceNotification" // TODO
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("GeofenceNotification", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(context, NotificationManager::class.java)!!
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
